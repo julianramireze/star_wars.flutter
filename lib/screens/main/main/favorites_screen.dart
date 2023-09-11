0d4a171a94d6e6c36e2558f526ff5f22c0bf0a08
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-11 10:47:24 -0500

    feature: favorites and reports screen basic.

--- lib/screens/main/main/favorites_screen.dart
+++ lib/screens/main/main/favorites_screen.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
+import 'package:star_wars/models/character.dart';
+import 'package:star_wars/stores/character.dart';
+import 'package:star_wars/widgets/character.dart';
+import 'package:star_wars/widgets/custom_button.dart';
+import 'package:star_wars/constants/colors.dart' as AppColors;
+import 'package:star_wars/utils/helpers/hooks.dart' as CustomHooks;
 
 class FavoritesScreen extends HookWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-    );
+    CustomHooks.useAutomaticKeepAlive(wantKeepAlive: true);
+    final characterStore = Provider.of<CharacterStore>(context);
+
+    return CustomButton(
+        padding: EdgeInsets.all(0),
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: AppColors.Colors.black,
+          resizeToAvoidBottomInset: true,
+          body: Column(children: [
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics()),
+                      itemCount: characterStore.charactersFavorites.length,
+                      itemBuilder: (context, index) {
+                        CharacterModel character =
+                            characterStore.charactersFavorites[index];
+                        return Character(
+                          character: character,
+                          isFavorite: CharacterFavoriteType.favorite,
+                          isReported: CharacterReportType.none,
+                        );
+                      },
+                    )))
+          ]),
+        ));
   }
 }
--- lib/screens/main/main/reports_screen.dart
+++ lib/screens/main/main/reports_screen.dart
@@ -1,13 +1,52 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:star_wars/models/character.dart';
+import 'package:star_wars/stores/character.dart';
+import 'package:star_wars/widgets/character.dart';
+import 'package:star_wars/widgets/custom_button.dart';
+import 'package:star_wars/constants/colors.dart' as AppColors;
+import 'package:star_wars/widgets/custom_input.dart';
+import 'package:star_wars/utils/helpers/hooks.dart' as CustomHooks;
 
 class ReportsScreen extends HookWidget {
   const ReportsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-    );
+    CustomHooks.useAutomaticKeepAlive(wantKeepAlive: true);
+    final characterStore = Provider.of<CharacterStore>(context);
+
+    return CustomButton(
+        padding: EdgeInsets.all(0),
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: AppColors.Colors.black,
+          resizeToAvoidBottomInset: true,
+          body: Column(children: [
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics()),
+                      itemCount: characterStore.charactersReported.length,
+                      itemBuilder: (context, index) {
+                        CharacterModel character =
+                            characterStore.charactersReported[index];
+                        return Character(
+                          character: character,
+                          isFavorite: CharacterFavoriteType.none,
+                          isReported: CharacterReportType.report,
+                        );
+                      },
+                    )))
+          ]),
+        ));
   }
 }
