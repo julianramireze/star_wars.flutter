567c785a7795ae3b76e3e67771f8f5b9e80d2811
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-12 11:57:39 -0500

    fix: add jsonplaceholder routes.

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,12 +1,15 @@
 class Api {
   static const _server = "https://swapi.dev/";
   static const _serverGoogle = "https://www.google.com/";
+  static const _serverJsonPlaceholder = "https://jsonplaceholder.typicode.com/";
 
   static get getServer => _server;
   static get getServerGoogle => _serverGoogle;
+  static get getServerJsonPlaceholder => _serverJsonPlaceholder;
 
   static const baseUrl = "${_server}api/";
-  static const baseUrlGoogleSearch = "${_serverGoogle}search";
+  static const baseUrlGoogleSearch = "${_serverGoogle}search/";
+  static const baseUrlJsonPlaceholder = _serverJsonPlaceholder;
 
   //endpoints for api
   static const endpointPeople = "people/";
