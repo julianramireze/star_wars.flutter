17b8f7a3b4a23c06c0b9bf2522e85da2ab236ef6
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-12 12:29:08 -0500

    feature: add report to CharacterService.

--- lib/services/character.dart
+++ lib/services/character.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:star_wars/config/api.dart';
 import 'package:star_wars/services/api.dart';
@@ -5,4 +7,17 @@ import 'package:star_wars/services/api.dart';
 class CharacterService {
   static Future<Response<dynamic>> get(int page) =>
       api.get('${Api.endpointPeople}?page=$page');
+
+  static Future<Response<dynamic>> report(
+      int id, String dateTime, String characterName) {
+    final formattedApi = api;
+    formattedApi.options.headers['Content-Type'] = 'application/json';
+
+    return api.post('${Api.baseUrlJsonPlaceholder}posts',
+        data: json.encode({
+          "userId": id,
+          "dateTime": dateTime,
+          "characterName": characterName
+        }).toString());
+  }
 }
