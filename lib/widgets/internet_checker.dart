312f997b6795155e58cd55e88bbede3270a5ba3b
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-11 19:51:23 -0500

    fix: improve offline text.

--- lib/widgets/internet_checker.dart
+++ lib/widgets/internet_checker.dart
@@ -4,6 +4,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
+import 'package:star_wars/stores/settings.dart';
 
 class InternetChecker extends HookWidget {
   final Widget child;
@@ -15,6 +17,7 @@ class InternetChecker extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final settingsStore = Provider.of<SettingsStore>(context);
     final connectivityStatus = useState<String>('Unknown');
 
     useEffect(() {
@@ -38,7 +41,7 @@ class InternetChecker extends HookWidget {
       Expanded(child: child),
       if (connectivityStatus.value == "none")
         Container(
-          color: Colors.black,
+          color: settingsStore.darkMode ? Colors.green : Colors.green[700]!,
           padding: EdgeInsets.only(top: 3, bottom: 3),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -48,11 +51,9 @@ class InternetChecker extends HookWidget {
                   child: Text(
                       tr(connectivityStatus.value != "none"
                           ? "again_with_internet"
-                          : "without_internet"),
+                          : "mode_offline"),
                       style: TextStyle(
-                          color: connectivityStatus.value != "none"
-                              ? Colors.green
-                              : Colors.redAccent,
+                          color: Theme.of(context).colorScheme.onSurface,
                           fontSize: 11)))
             ],
           ),
