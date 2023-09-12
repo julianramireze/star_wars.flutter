9f060b4021d1250cc0bfdee6cc8e5d428680d9a5
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-11 19:50:08 -0500

    feature: add google webiew link to search.

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,9 +1,12 @@
 class Api {
   static const _server = "https://swapi.dev/";
+  static const _serverGoogle = "https://www.google.com/";
 
   static get getServer => _server;
+  static get getServerGoogle => _serverGoogle;
 
   static const baseUrl = "${_server}api/";
+  static const baseUrlGoogleSearch = "${_serverGoogle}search";
 
   //endpoints for api
   static const endpointPeople = "people/";
