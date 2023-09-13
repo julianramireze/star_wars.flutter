76329ea5bf45f85d5aafb495539072bba1e81eaa
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-13 01:57:46 -0500

    feature: planet prompt.

--- lib/constants/prompts.dart
+++ lib/constants/prompts.dart
@@ -2,4 +2,8 @@ class Prompts {
   static String character(String name, String background) {
     return "A centered photograph in the foreground of $name from Star Wars, background $background, disney pixar style";
   }
+
+  static String planet(String terrain, String climate) {
+    return "Background about horizon of planet with terrain $terrain and climate $climate";
+  }
 }
