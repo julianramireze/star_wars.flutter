aa711877fa2ecaf9cc3588a75d1f5673895c28a4
Julian Ramirez <julian.ramirez.enciso@gmail.com>
2023-09-13 01:59:51 -0500

    fix: remove not necessary config.

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,9 +4,7 @@ import 'package:star_wars/config/api.dart';
 final _dio = Dio(
   BaseOptions(
     baseUrl: Api.baseUrl,
-    connectTimeout: const Duration(seconds: 20),
-    sendTimeout: const Duration(seconds: 20),
-    receiveTimeout: const Duration(seconds: 20),
+    connectTimeout: const Duration(seconds: 10),
     headers: {
       'Accept': '*/*',
       'Connection': 'keep-alive',
